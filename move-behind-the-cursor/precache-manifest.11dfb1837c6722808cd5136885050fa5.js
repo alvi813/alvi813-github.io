e9e1b0ed8cd89cfe40f6e3349ff65584d8b9ccda
Victoriya <alvi813@yandex.ru>
2020-07-28 04:16:47 +0300

    add move-behind-the-cursor project

---
+++ move-behind-the-cursor/precache-manifest.11dfb1837c6722808cd5136885050fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e962f53243132be26b490da3e28e350",
+    "url": "/move-behind-the-cursor/index.html"
+  },
+  {
+    "revision": "2aca6f152b8c572b2126",
+    "url": "/move-behind-the-cursor/static/css/main.983a2e11.chunk.css"
+  },
+  {
+    "revision": "f3bf0257e812b147bf28",
+    "url": "/move-behind-the-cursor/static/js/2.eda2e978.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/move-behind-the-cursor/static/js/2.eda2e978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aca6f152b8c572b2126",
+    "url": "/move-behind-the-cursor/static/js/main.3a62fe97.chunk.js"
+  },
+  {
+    "revision": "960df84e1cf6d650907f",
+    "url": "/move-behind-the-cursor/static/js/runtime-main.f67884d7.js"
+  }
+]);
