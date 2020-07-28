00fc0c7cafe843fc298d2902fc52dd641889184a
Victoriya <alvi813@yandex.ru>
2020-07-28 03:06:49 +0300

    add input-calc project

---
+++ input-calc/precache-manifest.652900982f572cea9770a15e007eeb8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d1b6b093b1cde239e115465d97ae86",
+    "url": "/input-calc/index.html"
+  },
+  {
+    "revision": "2fb2999703b7619b58f1",
+    "url": "/input-calc/static/css/main.464deb6b.chunk.css"
+  },
+  {
+    "revision": "bc12f8bd53881eb4e7f9",
+    "url": "/input-calc/static/js/2.b5286ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/input-calc/static/js/2.b5286ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb2999703b7619b58f1",
+    "url": "/input-calc/static/js/main.54a1635d.chunk.js"
+  },
+  {
+    "revision": "689fc5cee9a5f40fe5b7",
+    "url": "/input-calc/static/js/runtime-main.ad1b2750.js"
+  }
+]);
