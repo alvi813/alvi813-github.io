d146b4409a9729b584a68651dce3f56553235310
Victoriya <alvi813@yandex.ru>
2020-07-28 17:27:34 +0300

    add portfolio project

---
+++ portfolio/precache-manifest.128504747a79de556138492a4dc3b244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cdb0ae3d88c0bd0c528895809fcaf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ffa332b04fcff665b0",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3a96fc2b3d4b63fbd487",
+    "url": "/static/css/main.be14cffa.chunk.css"
+  },
+  {
+    "revision": "02ffa332b04fcff665b0",
+    "url": "/static/js/2.964be10a.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.964be10a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a96fc2b3d4b63fbd487",
+    "url": "/static/js/main.62f30981.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
