a6d62da32af87bc563ae0439ceb3f234316466a1
Victoriya <alvi813@yandex.ru>
2020-07-28 03:57:39 +0300

    add shopping-list project

---
+++ shopping-list/precache-manifest.e1b133143f55c19fcf431114f4f9435f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f71c3720b76ec2e11433cf761ce4e6",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "13da9df1589fa700e76e",
+    "url": "/shopping-list/static/css/main.1c006ec9.chunk.css"
+  },
+  {
+    "revision": "475f078de81458559c9a",
+    "url": "/shopping-list/static/js/2.04c2e2c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-list/static/js/2.04c2e2c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13da9df1589fa700e76e",
+    "url": "/shopping-list/static/js/main.0d14f9de.chunk.js"
+  },
+  {
+    "revision": "5adaedad1e12fa99d4cc",
+    "url": "/shopping-list/static/js/runtime-main.a80244fc.js"
+  }
+]);
