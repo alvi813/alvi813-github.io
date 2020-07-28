b88a1bb45a943291661e92258ef663b2f058e3d9
Victoriya <alvi813@yandex.ru>
2020-07-28 04:05:54 +0300

    add redux-edit-list project

---
+++ redux-edit-list/precache-manifest.154477d054681e8856a7efecce360c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea893969fc4d91680060b0487224476b",
+    "url": "/redux-edit-list/index.html"
+  },
+  {
+    "revision": "27fe1e465aa8bf898fa3",
+    "url": "/redux-edit-list/static/css/main.e113e7ae.chunk.css"
+  },
+  {
+    "revision": "b8c2b3cd762cc962bde3",
+    "url": "/redux-edit-list/static/js/2.fbf4e28f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-edit-list/static/js/2.fbf4e28f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27fe1e465aa8bf898fa3",
+    "url": "/redux-edit-list/static/js/main.b6ab7cbe.chunk.js"
+  },
+  {
+    "revision": "c09e9f63f20f221f4237",
+    "url": "/redux-edit-list/static/js/runtime-main.1a6e57af.js"
+  }
+]);
