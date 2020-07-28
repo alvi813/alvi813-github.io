9cfed8f818502009125c4787e17824af5e9e0a10
Victoriya <alvi813@yandex.ru>
2020-07-28 03:35:13 +0300

    add classical-calc project

---
+++ classical-calc/precache-manifest.17ba815010e2bcca933ee230b20477b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a53f828377dac306b7e5072d17d3ae",
+    "url": "/classical-calc/index.html"
+  },
+  {
+    "revision": "fb6114476748d6e6d80a",
+    "url": "/classical-calc/static/css/main.cec3b6b1.chunk.css"
+  },
+  {
+    "revision": "55c11a3b5af818780534",
+    "url": "/classical-calc/static/js/2.1262bdac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/classical-calc/static/js/2.1262bdac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6114476748d6e6d80a",
+    "url": "/classical-calc/static/js/main.b917d5da.chunk.js"
+  },
+  {
+    "revision": "0a6046590aea4564bd24",
+    "url": "/classical-calc/static/js/runtime-main.94d9173b.js"
+  }
+]);
